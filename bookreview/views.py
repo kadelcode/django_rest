5130db6976ef09803565f913620b4806c7e9910f
kadelcode <kadelcode@gmail.com>
2022-09-13 23:35:19 +0100

    Setting up the AuthorView on the web

--- bookreview/views.py
+++ bookreview/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import serializers, generics
+from rest_framework.response import Response
+from .models import Author
+from .serializers import AuthorSerializer
+from rest_framework.generics import ListAPIView
 
 # Create your views here.
+class AuthorView(ListAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
